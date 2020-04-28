d7e9e7cbfac193c2c846353f76a60cce4c23855a
keXXpert <kexxpert@ya.ru>
2020-04-28 16:28:44 +0300

    Added myCSS name for local css reffer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import classes from './Header.module.css';
+import myCSS from './Header.module.css';
 
 const Header = () => {
-    return <header className={classes.header}>
+    return <header className={myCSS.header}>
         <img src='https://dynamic.brandcrowd.com/asset/logo/523ee409-81f7-4d7a-900d-81e1e2473a2b/logo?v=4'></img>
     </header>
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import classes from './Profile.module.css';
+import myCSS from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return <main className={classes.profile}>
+    return <main className={myCSS.profile}>
         <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
         <div>
             avatar + description
