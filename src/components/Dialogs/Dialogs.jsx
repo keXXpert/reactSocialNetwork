8d9d53e9033f28663ffbdd60f03328cc5d0aea86
keXXpert <my@email.com>
2020-04-29 10:35:11 +0300

    added different pages, Route and links

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import myCSS from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return <main>
+        Dialogs
+    </main>
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import myCSS from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
-const Profile = () => {
-    return <main className={myCSS.profile}>
+const Profile = (props) => {
+    return <main>
         <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
         <div>
             avatar + description
