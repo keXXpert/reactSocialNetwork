56ca8feebd3cd28bfeac5a0ab5a30ae077a626f0
keXXpert <my@email.com>
2020-04-29 12:52:05 +0300

    added dialogs and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import myCSS from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+    let path = '/dialogs/id' + props.id;
+
+    return (
+        <div className={myCSS.dialog + ' ' + myCSS.active}>
+            <NavLink to={path} activeClassName={myCSS.active}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={myCSS.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
-    return <main>
-        Dialogs
-    </main>
+    return (
+        <main className={myCSS.dialogs}>
+            <div className={myCSS.dialogList}>
+                <DialogItem name='Alex' id='1' />
+                <DialogItem name='Matt' id='2' />
+                <DialogItem name='Erica' id='3' />
+                <DialogItem name='Beck' id='4' /> 
+                <DialogItem name='Kate' id='5' />
+            </div>
+            <div className={myCSS.messages}>
+                <Message message='Hey' />
+                <Message message='How are you?' />
+                <Message message='Yo' />
+            </div>
+        </main>
+    )
 }
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,19 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-    <div>
-        My Posts
-        <div>New Post</div>
-        <div className={myCSS.posts}>
-            <Post message='Hey! How are you?' likes='5' />
-            <Post message="It's my first post" likes='20' />
-            <Post />
+        <div>
+            My Posts
+            <div>
+            New Post
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={myCSS.posts}>
+                <Post message='Hey! How are you?' likes='5' />
+                <Post message="It's my first post" likes='20' />
+                <Post />
+            </div>
         </div>
-    </div>
     )
 }
 
