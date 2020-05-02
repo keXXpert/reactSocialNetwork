5b9d6fea2a98e0b25d536968768d89f5140f6619
keXXpert <my@email.com>
2020-05-02 10:30:26 +0300

    added switch case

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,16 +1,19 @@
 const dialogsReducer = (state, action) => {
-    if (action.type === 'SEND-MESSAGE') {
-        let newMessage = {
-            id: state.messages.length + 1,
-            text: state.newMessageText
-        }
-    
-        state.messages.push(newMessage);
-        state.newMessageText = '';
-    } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
-        state.newMessageText = action.text;
+    switch (action.type) {
+        case 'SEND-MESSAGE':
+            let newMessage = {
+                id: state.messages.length + 1,
+                text: state.newMessageText
+            }
+            state.messages.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case 'UPDATE-NEW-MESSAGE-TEXT':
+            state.newMessageText = action.text;
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
 
 export default dialogsReducer;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,19 +1,18 @@
 const profileReducer = (state, action) => {
-
-    if (action.type === 'ADD-POST') {
-        let newPost = {
-            id: state.posts.length + 1,
-            text: state.newPostText,
-            likes: 0
-        }
-    
-        state.posts.push(newPost);
-        state.newPostText = '';
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-        state.newPostText = action.text;
+    switch (action.type) {
+        case 'ADD-POST':
+            let newPost = {
+                id: state.posts.length + 1,
+                text: state.newPostText,
+                likes: 0
+            }
+            return state;
+        case 'UPDATE-NEW-POST-TEXT':
+            state.newPostText = action.text;
+            return state;
+        default:
+            return state;
     }
-
-    return state;
 }
 
 export default profileReducer;
