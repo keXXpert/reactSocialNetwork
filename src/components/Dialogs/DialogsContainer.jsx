21a7df1f762e18a63ac3c79d55aef6cd8bcf2bcc
keXXpert <my@email.com>
2020-05-04 10:31:39 +0300

    added react-redux

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+// import React from 'react';
 import Dialogs from './Dialogs';
+import { connect } from 'react-redux';
 
-const DialogsContainer = (props) => {
-    const newMessageUpdate = (text) => {
-        let action = {
-            type: 'UPDATE-NEW-MESSAGE-TEXT',
-            text: text
-        }
-        props.dispatch(action);
-    }
+// const DialogsContainer = (props) => {
+//     const newMessageUpdate = (text) => {
+//         let action = {
+//             type: 'UPDATE-NEW-MESSAGE-TEXT',
+//             text: text
+//         }
+//         props.dispatch(action);
+//     }
      
-    const sendMessage = () => {
-        props.dispatch({type: 'SEND-MESSAGE'});
-    }
+//     const sendMessage = () => {
+//         props.dispatch({type: 'SEND-MESSAGE'});
+//     }
 
-    return (
-        <Dialogs 
-            sendMessage={sendMessage}
-            newMessageUpdate={newMessageUpdate}
-            dialogsPage={props.dialogsPage}
-        />
-    )
+//     return (
+//         <Dialogs 
+//             sendMessage={sendMessage}
+//             newMessageUpdate={newMessageUpdate}
+//             dialogsPage={props.dialogsPage}
+//         />
+//     )
+// }
+
+
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    } 
 }
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        newMessageUpdate: (text) => {
+            let action = {
+                type: 'UPDATE-NEW-MESSAGE-TEXT',
+                text: text
+            }
+            dispatch(action);
+        },
+        sendMessage: () => {
+            dispatch({type: 'SEND-MESSAGE'});
+        }
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
+
 
 export default DialogsContainer;
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+// import React from 'react';
 import MyPosts from './MyPosts';
+import { connect } from 'react-redux';
 
-const MyPostsContainer = (props) => {
-    let updateNewPostText = (text) => {
-        let action = {
-            type: 'UPDATE-NEW-POST-TEXT',
-            text: text
-        }
-        props.dispatch(action);
-    }
+// const MyPostsContainer = (props) => {
+//     return (
+//         <StoreContext.Consumer> 
+//             {(store) => {
+//             let state = store.getState();
+//             let updateNewPostText = (text) => {
+//                 let action = {
+//                     type: 'UPDATE-NEW-POST-TEXT',
+//                     text: text
+//                 }
+//                 store.dispatch(action);
+//             }
+        
+//             let addNewPost = () => {
+//                 store.dispatch({ type: 'ADD-POST' });
+//             }
+//             return (<MyPosts
+//                 updateNewPostText={updateNewPostText}
+//                 addNewPost={addNewPost} posts={state.profilePage.posts} newPostText={state.profilePage.newPostText} />)
+//         }
+//         }
+//         </StoreContext.Consumer>
+//     )
+// }
 
-    let addNewPost = () => { 
-        props.dispatch({type: 'ADD-POST'});
-    }
-
-    return (
-        <MyPosts updateNewPostText={updateNewPostText} addNewPost={addNewPost} posts={props.profilePage.posts} newPostText={props.profilePage.newPostText} />
-    )
+let mapStateToProps = (state) => {
+    return {
+        profilePage: state.profilePage
+    } 
 }
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text) => {
+            let action = {
+                type: 'UPDATE-NEW-POST-TEXT',
+                text: text
+            }
+            dispatch(action);
+        },
+        addNewPost: () => {
+            dispatch({ type: 'ADD-POST' });
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
