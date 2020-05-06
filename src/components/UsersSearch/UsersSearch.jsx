b262f6f1eb54f5d0aed52933b0d0434032dc62c5
keXXpert <my@email.com>
2020-05-06 18:42:12 +0300

    added real ajax request for users

--- src/components/UsersSearch/UsersSearch.jsx
+++ src/components/UsersSearch/UsersSearch.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import UserItem from './UserItem/UserItem';
+import * as axios from 'axios';
 
 const UsersSearch = (props) => {
+    if (props.users.length === 0) {
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => props.setUsers(response.data.items));
+    }
+
     return (
         <main>
-            {props.users.map( el => (<UserItem {...el} followUser={props.followUser} />))}
+            {props.users.map(el => (<UserItem {...el} followUser={props.followUser} />))}
         </main>
     )
 }
