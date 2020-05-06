5fb67ac99b2096d6223205afa47639addbddef2f
keXXpert <my@email.com>
2020-05-06 18:54:23 +0300

    added clean function for loading users

--- src/components/UsersSearch/UsersSearch.jsx
+++ src/components/UsersSearch/UsersSearch.jsx
@@ -3,14 +3,17 @@ import UserItem from './UserItem/UserItem';
 import * as axios from 'axios';
 
 const UsersSearch = (props) => {
-    if (props.users.length === 0) {
-        axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => props.setUsers(response.data.items));
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => props.setUsers(response.data.items));
+        }
     }
 
     return (
         <main>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(el => (<UserItem {...el} followUser={props.followUser} />))}
         </main>
     )
