02984a6af70ff1d4bbc4cf69333509edde237b9f
keXXpert <kexxpert@ya.ru>
2020-05-06 18:42:12 +0300

    added real ajax request for users

--- src/components/UsersSearch/UserItem/UserItem.jsx
+++ src/components/UsersSearch/UserItem/UserItem.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import myCSS from './UserItem.module.css'
+import userAvatar from './../../../assets/images/ava.png'
 
 const UserItem = (props) => {
     return (
         <div key={props.id}>
             <span>
                 <div>
-                    <img src={props.photoURL} className={myCSS.avatar}/>
+                    <img src={props.photos.small === null ? userAvatar : props.photos.small} className={myCSS.avatar}/>
                 </div>
                 <div>
-                    <button onClick={() => {props.followUser(props.id)}}>{props.followed ? 'Follow': 'Unfollow'} </button>
+                    <button onClick={() => {props.followUser(props.id)}}>{props.followed ? 'Unfollow': 'Follow'} </button>
                 </div>
             </span>
             <span>
                 <span>
-                    <div>{props.fullName}</div>
+                    <div>{props.name}</div>
                     <div>{props.status}</div>
                 </span>
                 <span>
-                    <div>{props.location.city}</div>
-                    <div>{props.location.country}</div>
+                    <div>{"props.location.city"}</div>
+                    <div>{"props.location.country"}</div>
                 </span>
             </span>
         </div>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,9 +3,9 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
     users: [
-        { id: 1, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: true, fullName: 'Dmitry K.', status: 'I\'m a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: false, fullName: 'Alex L.', status: 'I\'m a boss too', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: true, fullName: 'Andrew M.', status: 'I\'m a super boss', location: { city: 'Kiev', country: 'Ukraine' } }
+        // { id: 1, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: true, fullName: 'Dmitry K.', status: 'I\'m a boss', location: { city: 'Minsk', country: 'Belarus' } },
+        // { id: 2, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: false, fullName: 'Alex L.', status: 'I\'m a boss too', location: { city: 'Moscow', country: 'Russia' } },
+        // { id: 3, photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg', followed: true, fullName: 'Andrew M.', status: 'I\'m a super boss', location: { city: 'Kiev', country: 'Ukraine' } }
     ],
     usersOnPage: 4
 }
