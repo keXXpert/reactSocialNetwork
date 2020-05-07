92445bc863e142561df5b6021d9aa8375134c385
keXXpert <kexxpert@ya.ru>
2020-05-07 19:41:26 +0300

    added class component

--- src/components/UsersSearch/UsersSearch.jsx
+++ src/components/UsersSearch/UsersSearch.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import UserItem from './UserItem/UserItem';
 import * as axios from 'axios';
 
-const UsersSearch = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
+class UsersSearch extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length === 0) {
             axios
                 .get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => props.setUsers(response.data.items));
+                .then(response => this.props.setUsers(response.data.items));
         }
     }
 
-    return (
-        <main>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map(el => (<UserItem {...el} followUser={props.followUser} />))}
+    render() {
+        return (<main>
+            <button onClick={this.getUsers}>Get Users</button>
+            {this.props.users.map(el => (<UserItem {...el} followUser={this.props.followUser} />))}
         </main>
-    )
+        )
+    }
 }
 
 export default UsersSearch;
