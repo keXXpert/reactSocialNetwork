a4bd4dd351eb083b4220b9f01ebafe1de1d92077
Artem <my@email.com>
2020-05-16 11:10:45 +0300

    added redux-form library

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import myCSS from './Login.module.css';
+import { reduxForm, Field } from 'redux-form';
+
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field placeholder='Login' name='login' component='input' />
+        </div>
+        <div>
+            <Field placeholder='Password' name='password' component='input' />
+        </div>
+        <div>
+            <Field component='input' name='rememberMe' type={'checkbox'} />remember me
+        </div>
+        <div>
+            <button>Log in</button>
+        </div>
+    </form>
+}
+
+
+const ReduxLoginForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
-    return <main>
-        Login page
-    </main>
+    const onSubmit = (formData) => {
+        debugger;
+    }
+    
+    return <div>
+        <h3>Login</h3>
+        <ReduxLoginForm onSubmit={onSubmit} />
+    </div>
 }
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import dialogsReducer from "./dialogsReducer";
 import searchUsersReducer from "./usersReducer";
 import authReducer from './authReducer';
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profilePage: profileReducer,  
     dialogsPage: dialogsReducer,
     usersPage: searchUsersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
