f5a356d984b24d20b71c950b9a619d0e57b47156
keXXpert <my@email.com>
2020-05-23 18:29:41 +0300

    added test

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/redux-store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/profileReducer.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import profileReducer, { addNewPost } from './profileReducer'
+
+let state = {
+    profile: null,
+    posts: [
+        { id: 1, text: 'Hey! How are you?', likes: 5 },
+        { id: 2, text: 'It\'s my first post', likes: 20 }
+    ]
+}
+
+test('posts array length sould increment', () => {
+    // test data
+    let action = addNewPost('New test post')
+
+    // action
+    let newState = profileReducer(state, action)
+
+    //expectetion
+    expect(newState.posts.length).toBe(3)
+});
+
+test('new post message should be correct', () => {
+    // test data
+    let action = addNewPost('New test post')
+
+    // action
+    let newState = profileReducer(state, action)
+
+    //expectetion
+    expect(newState.posts[2].text).toBe('New test post')
+});
